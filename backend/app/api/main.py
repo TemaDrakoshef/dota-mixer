562ddc18f9c48ad112fef7d6a30f4abdb552a64f
TemaDrakoshef <artem125149@gmail.com>
2024-06-08 17:12:10 +0300

    🔧 Add getting heroes with OpenDota

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, users, utils
+from app.api.routes import items, heroes, login, users, utils
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(heroes.router, prefix="/heroes", tags=["heroes"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
